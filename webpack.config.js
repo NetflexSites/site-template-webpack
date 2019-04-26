17eadfb16e27a983b25d432341d9a09c0e811859
Johan Vaage <jovaage@gmail.com>
2019-04-26 13:45:32 +0200

    Adds eslint with standardjs as ruleset

--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,8 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: [
-          'babel-loader'
+          'babel-loader',
+          'eslint-loader'
         ]
       },
       {
