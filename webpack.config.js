f15ba1f4141fce5c6f036e7703849351f766600b
Johan Vaage <jovaage@gmail.com>
2019-04-26 13:39:02 +0200

    Moves config out of package, into separate files

--- webpack.config.js
+++ webpack.config.js
@@ -1,60 +1,53 @@
-const path = require('path');
+const path = require('path')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const autoprefixer = require('autoprefixer')
-const package = require('./package.json')
 
 module.exports = {
   entry: [
     path.resolve(__dirname, 'assets/js/app.js'),
     path.resolve(__dirname, 'assets/scss/styles.scss')
   ],
+
   performance: {
     hints: false
   },
+
   stats: {
     all: false,
     assets: true,
     chunks: false
   },
+
   devtool: 'source-map',
+
   mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+
   plugins: [
     new MiniCssExtractPlugin({
       filename: 'css/[name].css'
     })
   ],
+
   output: {
     filename: 'js/[name].js',
     path: path.resolve(__dirname, 'public/assets')
   },
+
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
-        options: {
-          babelrc: true
-        }
+        use: [
+          'babel-loader'
+        ]
       },
       {
         test: /\.(sa|sc|c)ss$/,
         use: [
           MiniCssExtractPlugin.loader,
-          { loader: 'css-loader' },
-          { loader: 'sass-loader' },
-          {
-            loader: 'postcss-loader',
-            options: {
-              plugins: [
-                autoprefixer({
-                  grid: true,
-                  flexbox: true,
-                  browsers: package.browserslist
-                })
-              ]
-            }
-          }
+          'css-loader',
+          'postcss-loader',
+          'sass-loader'
         ]
       }
     ]
