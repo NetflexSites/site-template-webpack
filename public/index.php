11307c7cd64215eea061da24e4cf2b47febe25e0
Thomas Alrek <thomas@alrek.no>
2019-05-20 14:27:38 +0200

    Update to use NetflexSDK

--- public/index.php
+++ public/index.php
@@ -1,10 +1,4 @@
 <?php
 
 require_once(__DIR__ . '/../vendor/autoload.php');
-
-/* $whoops = new Whoops\Run;
-$whoops->pushHandler(new Whoops\Handler\PrettyPageHandler);
-$whoops->register(); */
-
-// Initialize Netflex Site Framework
-require_once(__DIR__ . '/../vendor/netflex/siteV3/bootstrap.php');
+require_once(Netflex\SDK::bootstrap);
