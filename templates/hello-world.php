1ed6cdeb14ff94580ca27b61e4851eab26d6226f
Thomas Alrek <thomas@alrek.no>
2019-07-18 19:56:58 +0200

    Update template with example

---
+++ templates/hello-world.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<? get_block('head') ?>
+<? global $page; ?>
+<body class="d-flex flex-column">
+  <main class="d-flex flex-column flex-grow-1 justify-content-center align-items-center">
+    <h1 class="mr-3 pr-3 align-top inline-block align-content-center">
+      Hello World!
+    </h1>
+    <div class="inline-block align-middle">
+      <h2 class="font-weight-normal lead">
+        Welcome to your new Netflex site.
+      </h2>
+    </div>
+    <div class="inline-block align-middle">
+      <p>To get started see the <a href="https://docs.netflex.dev">developer documentation</a></p>
+    </div>
+  </main>
+  <? get_block('footer') ?>
+</body>
+</html>
